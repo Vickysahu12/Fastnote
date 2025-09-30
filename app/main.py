0de5303ba9e42953339d1cf07d8d67c2ad62e958
Vickysahu12 <bikisahu161@gmail.com>
2025-09-30 20:42:20 +0530

    made the first version of FASTNOTES APPLICATION

--- app/main.py
+++ app/main.py
@@ -12,3 +12,22 @@ async def note_create(new_note:dict):
 async def note_get(note_id:int):
     note = await notes_services.get_note(note_id)
     return note
+
+@app.put("/notes/{note_id}")
+async def note_update(note_id:int, new_note:dict):
+    new_title = new_note.get("title")
+    new_content = new_note.get("content")
+    note = await notes_services.update_note(note_id, new_title, new_content)
+    return note
+
+@app.patch("/notes/{note_id}")
+async def note_patch(note_id:int, new_note:dict):
+    new_title = new_note.get("title")
+    new_content = new_note.get("content")
+    note = await notes_services.patch_note(note_id,new_title,new_content)
+    return note
+
+@app.delete("/notes/{note_id}")
+async def note_delete(note_id:int):
+    response = await notes_services.delete_note(note_id)
+    return response
