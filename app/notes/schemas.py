98a24767509351b4f154b5cb8c4441f10f841012
Vickysahu12 <bikisahu161@gmail.com>
2025-10-01 13:04:25 +0530

    Make some improvements in Code

---
+++ app/notes/schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, ConfigDict
+
+# Shared Base Fields
+class NoteBase(BaseModel):
+    title:str
+    content:str
+
+#  For Creation
+class NoteCreate(NoteBase):
+    pass
+
+# For Partial Update
+class NotePatch(BaseModel):
+    title:str | None = None
+    content:str | None = None
+
+class NoteUpdate(BaseModel):
+    title:str
+    content:str 
+
+class NoteOut(NoteBase):
+    id:int
+    model_config = ConfigDict(from_attributes=True)
--- app/notes/services.py
+++ app/notes/services.py
@@ -2,10 +2,11 @@ from app.db.config import async_session
 from app.notes.models import Note
 from sqlalchemy import select
 from fastapi import HTTPException
+from app.notes.schemas import NoteCreate,NoteUpdate,NotePatch
 
-async def create_note(title:str, content:str):
+async def create_note(new_note:NoteCreate):
     async with async_session() as session:
-        note = Note(title=title,content=content)
+        note = Note(title=new_note.title,content=new_note.content)
         session.add(note)
         await session.commit()
         await session.refresh(note)
@@ -24,28 +25,28 @@ async def get_all_notes():
         notes = await session.scalars(stmt)
         return notes.all()
     
-async def update_note(note_id:int, new_title:str, new_content:str):
+async def update_note(note_id:int, new_note:NoteUpdate):
     async with async_session() as session:
         note = await session.get(Note,note_id)
         if note is None:
             raise HTTPException(status_code=404, detail="Not Found")
-        note.title = new_title
-        note.content = new_content
+        note.title = new_note.title
+        note.content = new_note.content
         await session.commit()
         await session.refresh(note)
         return note
     
-async def patch_note(note_id:int, new_title:str | None=None, new_content:str | None = None):
+async def patch_note(note_id:int, new_note:NotePatch):
     async with async_session() as session:
         note = await session.get(Note, note_id)
         if note is None:
             raise HTTPException(status_code=404, detail="Note Not Found")
         
 
-        if new_title is not None:
-            note.title = new_title
-        if new_content is not None:
-            note.content = new_content
+        if new_note.title is not None:
+            note.title = new_note.title
+        if new_note.content is not None:
+            note.content = new_note.content
 
         await session.commit()
         await session.refresh(note)
